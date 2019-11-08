d4011c7a8ba81ea68c830503c6319b9e2bb31027
Tony Deng <wolf.deng@gmail.com>
2019-11-09 05:31:00 +0800

    :sparkles: feat(1.0): 添加函数中闭包的使用例子
    refs #1
    
    Signed-off-by: Tony Deng <wolf.deng@gmail.com>

--- src/ch02/function.rs
+++ src/ch02/function.rs
@@ -67,6 +67,100 @@ fn prooduct(a: i32, b: i32) -> i32 {
     a * b
 }
 
-fn is_true(a: i32, b: i32) -> bool {
-    true
+// fn is_true(a: i32, b: i32) -> bool {
+//     true
+// }
+
+/// # 闭包
+/// 
+/// Basic usage:
+/// 
+/// ```
+/// pub fn closure(){
+///    let out = 42;
+///    // 普通函数
+///    fn add(i: i32, j: i32) -> i32 {
+///        i + j
+///    }
+///    // 定义类型标注的闭包
+///    let closure_annotated = |i: i32, j: i32| -> i32 {i + j + out};
+///    // 如果没有类型标注，则由编译器推断类型
+///    let closure_interred = |i,j| i + j + out;
+///
+///    let i = 1;
+///    let j = 2;
+/// 
+///    assert_eq!(3, add(i, j));
+///    assert_eq!(45, closure_annotated(i, j));
+///    assert_eq!(45, closure_interred(i, j));
+///} 
+/// closure()
+/// ```
+pub fn closure(){
+    let out = 42;
+
+    fn add(i: i32, j: i32) -> i32 {
+        i + j
+    }
+
+    let closure_annotated = |i: i32, j: i32| -> i32 {i + j + out};
+    let closure_interred = |i,j| i + j + out;
+
+    let i = 1;
+    let j = 2;
+
+    assert_eq!(3, add(i, j));
+    assert_eq!(45, closure_annotated(i, j));
+    assert_eq!(45, closure_interred(i, j));
+}
+
+/// # 闭包: 作为参数
+/// 
+/// Basic usage:
+/// 
+/// ```
+/// pub fn closure_math<F: Fn() -> i32>(op: F) -> i32{
+///     op()
+/// }
+/// let a = 2;
+/// let b = 3;
+/// assert_eq!(closure_math(|| a + b), 5);
+/// assert_eq!(closure_math(|| a * b), 6);
+/// ```
+pub fn closure_math<F: Fn() -> i32>(op: F) -> i32 {
+    op()
+}
+
+/// # 闭包: 作为返回值（动态分发）2018
+/// 
+/// Basic usage:
+/// 
+/// ```
+/// pub fn two_times() -> Box<dyn Fn(i32) -> i32>{
+///     let i = 2;
+///     Box::new(move |j| j * i)
+/// }
+/// let result = two_times();
+/// assert_eq!(result(2),4)
+/// ```
+pub fn two_times() -> Box<dyn Fn(i32) -> i32> {
+    let i = 2;
+    Box::new(move |j| j * i)
+}
+
+/// # 闭包: 作为返回值（静态分发）
+/// 
+/// Basic usage:
+/// 
+/// ```
+/// fn two_times_impl() -> impl Fn(i32) -> i32{
+///     let i = 2;
+///     move |j| j * j
+/// }
+/// let result = two_times_impl();
+/// assert_eq!(result(2), 4);
+/// ```
+pub fn two_times_impl() -> impl Fn(i32) -> i32 {
+    let i = 2;
+    move |j| j * i
 }
